59dedcb6c8297754e3c6ba9c5a23906ce38ef26d
Sárándi <sarandi.balint@gmail.com>
2022-09-20 12:43:21 +0200

    Kívánatos szélső értékek

--- main.js
+++ main.js
@@ -2,40 +2,46 @@ function gombKatt(){
     let suly = parseFloat(document.getElementById('suly').value);
     let magassag = parseFloat(document.getElementById('magassag').value);
     let bmiErtek = suly / (magassag/100)**2;
-    document.getElementById("bmiertek").innerHTML = "AZ ÖN BMI ÉRTÉKE: " + bmiErtek.toFixed(2);
+    document.getElementById("bmiertek").innerHTML = "<b>AZ ÖN BMI ÉRTÉKE: </b>" + bmiErtek.toFixed(2);
 
     var element = document.getElementById("sovany");
     element.classList.remove("narancs");
-    var element = document.getElementById("normal");
+    element = document.getElementById("normal");
     element.classList.remove("narancs");
-    var element = document.getElementById("tulsuly");
+    element = document.getElementById("tulsuly");
     element.classList.remove("narancs");
-    var element = document.getElementById("elsofok");
+    element = document.getElementById("elsofok");
     element.classList.remove("narancs");
-    var element = document.getElementById("masodikfok");
+    element = document.getElementById("masodikfok");
     element.classList.remove("narancs");
-    var element = document.getElementById("harmadikfok");
+    element = document.getElementById("harmadikfok");
     element.classList.remove("narancs");
 
     if(bmiErtek.toFixed(2)<=18.5){
-        var element = document.getElementById("sovany");
+        element = document.getElementById("sovany");
         element.classList.add("narancs");
     }else if(bmiErtek.toFixed(2)<=24.9){
-        var element = document.getElementById("normal");
+        element = document.getElementById("normal");
         element.classList.add("narancs");
     }else if(bmiErtek.toFixed(2)<=29.9){
-        var element = document.getElementById("tulsuly");
+        element = document.getElementById("tulsuly");
         element.classList.add("narancs");
     }else if(bmiErtek.toFixed(2)<=34.9){
-        var element = document.getElementById("elsofok");
+        element = document.getElementById("elsofok");
         element.classList.add("narancs");
     }else if(bmiErtek.toFixed(2)<=39.9){
-        var element = document.getElementById("masodikfok");
+        element = document.getElementById("masodikfok");
         element.classList.add("narancs");
     }else{
-        var element = document.getElementById("harmadikfok");
+        element = document.getElementById("harmadikfok");
         element.classList.add("narancs");
     }
+
+    let also = (18.5*(magassag/100)**2).toFixed(1);
+    let felso = (24.9*((magassag/100)**2)).toFixed(1);
+    var atlag = ((also + felso)/2);
+    document.getElementById("idealis").innerHTML = "<b>AZ IDEÁLIS TESTTÖMEGE: </b>" + atlag + " kg";
+    document.getElementById("szelso").innerHTML = also + " - " + felso + " kg";
 }
 
 
